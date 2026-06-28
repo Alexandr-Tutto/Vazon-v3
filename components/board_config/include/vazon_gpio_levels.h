df0d10aa48cbf9cea231c6d8ed9aea2c2c488955
Alexandr-Tutto <58428278+Alexandr-Tutto@users.noreply.github.com>
2026-06-28 15:28:05 +0300

    firmware: add GPIO level definitions

---
+++ components/board_config/include/vazon_gpio_levels.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "driver/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VAZON_GPIO_LEVEL_LOW   0
+#define VAZON_GPIO_LEVEL_HIGH  1
+
+#define VAZON_OUTPUT_ACTIVE_LOW_ON_LEVEL   VAZON_GPIO_LEVEL_LOW
+#define VAZON_OUTPUT_ACTIVE_LOW_OFF_LEVEL  VAZON_GPIO_LEVEL_HIGH
+
+#define VAZON_DOOR_CLOSED_LEVEL            VAZON_GPIO_LEVEL_LOW
+#define VAZON_DOOR_OPEN_LEVEL              VAZON_GPIO_LEVEL_HIGH
+
+#define VAZON_WATER_EMPTY_LEVEL            VAZON_GPIO_LEVEL_LOW
+#define VAZON_WATER_PRESENT_LEVEL          VAZON_GPIO_LEVEL_HIGH
+
+#ifdef __cplusplus
+}
+#endif
